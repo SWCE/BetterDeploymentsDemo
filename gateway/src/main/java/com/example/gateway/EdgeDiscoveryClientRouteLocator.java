8b84a1c4916f7419366fbe60e3e04ef228d14216
rezra3 <roi.ezra@intel.com>
2018-11-28 08:21:36 +0200

    code cleanup

--- gateway/src/main/java/com/example/gateway/EdgeDiscoveryClientRouteLocator.java
+++ gateway/src/main/java/com/example/gateway/EdgeDiscoveryClientRouteLocator.java
@@ -1,5 +1,7 @@
 package com.example.gateway;
 
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import java.util.Collection;
@@ -11,6 +13,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
@@ -21,15 +24,15 @@ import org.springframework.cloud.netflix.zuul.filters.discovery.ServiceRouteMapp
 public class EdgeDiscoveryClientRouteLocator extends DiscoveryClientRouteLocator {
 
 
-    private final ConsulWithTagsDiscoveryClient discoveryClient;
+    private final ConsulClient consulClient;
     private final ZuulProperties zuulProperties;
     private final Set<String> ignoredServices;
 
     public EdgeDiscoveryClientRouteLocator(String servletPath, ZuulProperties zuulProperties,
-        ServiceRouteMapper serviceRouteMapper, ConsulWithTagsDiscoveryClient discoveryClient,
-        Optional<Registration> registration) {
+        ServiceRouteMapper serviceRouteMapper, DiscoveryClient discoveryClient, Optional<Registration> registration,
+        ConsulClient consulClient) {
         super(servletPath, discoveryClient, zuulProperties, serviceRouteMapper, registration.orElse(null));
-        this.discoveryClient = discoveryClient;
+        this.consulClient = consulClient;
         this.zuulProperties = zuulProperties;
         this.ignoredServices = ImmutableSet.copyOf(zuulProperties.getIgnoredServices());
     }
@@ -37,19 +40,21 @@ public class EdgeDiscoveryClientRouteLocator extends DiscoveryClientRouteLocator
     @Override
     protected LinkedHashMap<String, ZuulRoute> locateRoutes() {
         zuulProperties.setIgnoredServices(Sets.newHashSet(ignoredServices));
-        zuulProperties.getIgnoredServices().addAll(getExcludedServices().keySet());
+        zuulProperties.getIgnoredServices().addAll(getExcludedServices());
         log.info("Ignored services are: {}", zuulProperties.getIgnoredServices());
         return super.locateRoutes();
     }
 
-    private Map<String, List<String>> getExcludedServices() {
-        return discoveryClient.getConsulServices().entrySet().stream()
-            .filter(service -> !isEdgeService(service.getValue()))
-            .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+    private List<String> getExcludedServices() {
+        return getConsulServices().entrySet().stream().filter(service -> !isEdgeService(service.getValue()))
+            .map(Entry::getKey).collect(Collectors.toList());
     }
 
     private static boolean isEdgeService(Collection<String> tags) {
         return tags.contains("EdgeService");
     }
 
+    private Map<String, List<String>> getConsulServices() {
+        return consulClient.getCatalogServices(QueryParams.DEFAULT).getValue();
+    }
 }
--- gateway/src/main/java/com/example/gateway/GatewayConfiguration.java
+++ gateway/src/main/java/com/example/gateway/GatewayConfiguration.java
@@ -6,18 +6,14 @@ import com.example.gateway.ribbon.TagRoundRobinRule;
 import com.netflix.loadbalancer.IRule;
 import java.util.Optional;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.consul.ConditionalOnConsulEnabled;
-import org.springframework.cloud.consul.discovery.ConsulDiscoveryClientConfiguration;
-import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.discovery.ServiceRouteMapper;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 @RibbonClients(defaultConfiguration = DefaultRibbonConfig.class)
@@ -27,18 +23,11 @@ public class GatewayConfiguration {
     @ConditionalOnConsulEnabled
     public EdgeDiscoveryClientRouteLocator edgeDiscoveryClientRouteLocator(ServerProperties serverProperties,
         ZuulProperties zuulProperties, ServiceRouteMapper serviceRouteMapper, Optional<Registration> registration,
-        ConsulWithTagsDiscoveryClient discoveryClient) {
+        DiscoveryClient discoveryClient, ConsulClient consulClient) {
         return new EdgeDiscoveryClientRouteLocator(serverProperties.getServletPath(), zuulProperties,
-            serviceRouteMapper, discoveryClient, registration);
+            serviceRouteMapper, discoveryClient, registration, consulClient);
     }
 
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public ConsulWithTagsDiscoveryClient consulWithTagsDiscoveryClient(ConsulClient consulClient,
-        ConsulDiscoveryProperties consulDiscoveryProperties, ApplicationContext ctx) {
-        return new ConsulWithTagsDiscoveryClient(consulClient, consulDiscoveryProperties,
-            new ConsulDiscoveryClientConfiguration().new LifecycleRegistrationResolver(ctx));
-    }
 
     @Configuration
     class DefaultRibbonConfig {
